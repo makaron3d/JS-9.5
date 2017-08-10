0a1d45b497facb9ab33c3c6dd18aa282a47f3ff6
makaron3d <makaron3d@gmail.com>
2017-08-10 15:16:07 +0200

    001

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// test code
+var variable = 'alright';
+alert('alright')
+console.log(variable);
+/* 
+var variable = 'test';
+console.log(variable);
+*/
+var age = prompt('Enter your age');
+alert('Masz ' + age + ' lat!' );
+console.log('Hello, ');
