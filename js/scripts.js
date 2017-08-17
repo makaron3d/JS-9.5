0a925a2fdc365a0c1c9b900c56ce47771070e190
makaron3d <makaron3d@gmail.com>
2017-08-17 14:37:12 +0200

    022

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 var HTMLButtonElement = document.getElementsByClassName('button');
 console.log(HTMLButtonElement);
 var buttonsNumber = HTMLButtonElement.length;
-var buttonText = HTMLButtonElement.innerText;
-
-    for (var i = 0; i < buttonsNumber; i++) {
+   
+for (var i = 0; i < buttonsNumber; i++) {
         console.log(HTMLButtonElement + i);
-        console.log(buttonText);
-        }
+        console.log(HTMLButtonElement[i].innerText);
+}
 
